7591c1793f9d423e0ab269d3ff6db39e0c7ebd60
Luke <lukestevens@hotmail.com>
2025-02-14 16:01:50 +1100

    Major overhaul
    * Don't add links added to core for versions v15.0.3+
    * Add link for view record schedule
    * Add link to report A with record as live filter
    * Improved study config and permission checks

--- README.md
+++ README.md
@@ -7,9 +7,19 @@ Luke Stevens, Murdoch Children's Research Institute https://www.mcri.edu.au
 ********************************************************************************
 ## Summary of Functionality
 
-Adds links with record filter to the Record Actions dropdown of the Record Home page:
+Adds links with record filter to the Record Actions dropdown of the Record Home page. The links added are sensitive to project configuration and to the user's permissions.
+
+### Module Version 1.x.x (from 14-Mar-2022)
+Links added to dropdown:
 * Logging
 * Notification Log
 * Survey Invitation Log
+
+### Module Version 2.x.x (from 14-Feb-2025)
+Links added to dropdown:
+* View Schedule
+* Report A (All data)
+
+Links for record *logging*, *alerts*, and *survey invitations* were incorporated into REDCap as core functionality in version v15.0.3. The module will continue to add them for versions prior to v15.0.3.
 
 ********************************************************************************
--- RecordLoggingLink.php
+++ RecordLoggingLink.php
@@ -10,8 +10,12 @@ use ExternalModules\AbstractExternalModule;
 
 class RecordLoggingLink extends AbstractExternalModule
 {
+    const LINK_MARKUP = '<li class="ui-menu-item"><a target="_blank" href="||HREF||"  style="display:block;" tabindex="-1" role="menuitem" class="ui-menu-item-wrapper"><span style="vertical-align:middle;color:#000066;">||LABEL||<i class="fas fa-external-link-alt fs10 ml-2"></i></span></a></li>';
+    const HREFMARK = '||HREF||';
+    const LABELMARK = '||LABEL||';
+    
     public function redcap_every_page_top($project_id) {
-        global $user_rights,$lang;
+        global $user_rights,$Proj;
         if (!(PAGE==='DataEntry/record_home.php' && isset($_GET['id']))) return;
         try {
             $user = $this->getUser();
@@ -19,15 +23,70 @@ class RecordLoggingLink extends AbstractExternalModule
             return; // e.g. not logged in
         }
         $record = $this->escape($_GET['id']);
+        $arm = $this->escape($_GET['arm']);
         $links = array();
-        if ($user_rights['data_logging'] || $user->isSuperUser()) $links[] = '<li class="ui-menu-item"><a target="_blank" href="'.APP_PATH_WEBROOT.'Logging/index.php?pid='.$project_id.'&record='.$record.'"                                  style="display:block;" tabindex="-1" role="menuitem" class="ui-menu-item-wrapper"><span style="vertical-align:middle;color:#000066;"><i class="fas fa-receipt   mr-1"></i>'.\RCView::tt('app_07'    ).'<i class="fas fa-external-link-alt ml-1"></i></span></a></li>';
-        if ($user_rights['alerts']       || $user->isSuperUser()) $links[] = '<li class="ui-menu-item"><a target="_blank" href="'.APP_PATH_WEBROOT.'index.php?pid='.$project_id.'&route=AlertsController:setup&log=1&filterRecord='.$record.'" style="display:block;" tabindex="-1" role="menuitem" class="ui-menu-item-wrapper"><span style="vertical-align:middle;color:#000066;"><i class="fas fa-table     mr-1"></i>'.\RCView::tt('alerts_20' ).'<i class="fas fa-external-link-alt ml-1"></i></span></a></li>';
-        if ($user_rights['participants'] || $user->isSuperUser()) $links[] = '<li class="ui-menu-item"><a target="_blank" href="'.APP_PATH_WEBROOT.'Surveys/invite_participants.php?pid='.$project_id.'&email_log=1&filterRecord='.$record.'"  style="display:block;" tabindex="-1" role="menuitem" class="ui-menu-item-wrapper"><span style="vertical-align:middle;color:#000066;"><i class="fas fa-mail-bulk mr-1"></i>'.\RCView::tt('survey_350').'<i class="fas fa-external-link-alt ml-1"></i></span></a></li>';
+
+        if (\REDCap::versionCompare(REDCAP_VERSION,'15.0.2','<=') && ($user_rights['data_logging'] || $user->isSuperUser())) {
+            $links[] = array(
+                'href' => APP_PATH_WEBROOT.'Logging/index.php?pid='.$project_id.'&record='.$record,
+                'label' => '<i class="fas fa-receipt   mr-1"></i>'.\RCView::tt('app_07')
+            );
+        }
+        
+        if (\REDCap::versionCompare(REDCAP_VERSION,'15.0.2','<=') && ($user_rights['alerts'] || $user->isSuperUser())) {
+            $links[] = array(
+                'href' => APP_PATH_WEBROOT.'index.php?pid='.$project_id.'&route=AlertsController:setup&log=1&filterRecord='.$record,
+                'label' => '<i class="fas fa-table mr-1"></i>'.\RCView::tt('alerts_20')
+            );
+        }
+
+        if (\REDCap::versionCompare(REDCAP_VERSION,'15.0.2','<=') && $Proj->project['surveys_enabled'] && ($user_rights['participants'] || $user->isSuperUser())) {
+            $links[] = array(
+                'href' => APP_PATH_WEBROOT.'Surveys/invite_participants.php?pid='.$project_id.'&email_log=1&filterRecord='.$record,
+                'label' => '<i class="fas fa-mail-bulk mr-1"></i>'.\RCView::tt('survey_350')
+            );
+        }
+
+        if ($Proj->project['scheduling'] && ($user_rights['calendar'] || $user->isSuperUser())) {
+            // Does the record have a schedule already?
+            $sql = "select count(*) as event_count
+                    from redcap_events_calendar ec
+                    inner join redcap_events_metadata em on ec.event_id=em.event_id
+                    inner join redcap_events_arms ea on em.arm_id=ea.arm_id and ec.project_id=ea.project_id
+                    where ec.project_id = ? and record = ? and arm_num = ?";
+            $result = $this->query($sql, [$project_id, $record, $arm])->fetch_assoc();
+
+            if ($result['event_count'] == 0) {
+                //scheduling_03 = "Create Schedule"
+                $links[] = array(
+                    'href' => APP_PATH_WEBROOT.'Calendar/scheduling.php?pid='.$project_id,
+                    'label' => '<i class="fas fa-mail-bulk mr-1"></i>'.\RCView::tt('scheduling_03')
+                );
+            } else {
+                //scheduling_04 = "View or Edit Schedule"
+                $links[] = array(
+                    'href' => APP_PATH_WEBROOT.'Calendar/scheduling.php?pid='.$project_id.'&record='.$record.'&arm='.$arm,
+                    'label' => '<i class="fas fa-mail-bulk mr-1"></i>'.\RCView::tt('scheduling_04')
+                );
+            }
+        }
+
+        if ($user_rights['reports'] || $user->isSuperUser()) {
+            // report_builder_44 = "View Report" report_builder_80 = "All data"
+            $links[] = array(
+                'href' => APP_PATH_WEBROOT.'DataExport/index.php?pid='.$project_id.'&report_id=ALL&pagenum=1&lf1='.$record,
+                'label' => '<i class="fas fa-mail-bulk mr-1"></i>'.\RCView::tt('report_builder_44').\RCView::tt('colon').' '.\RCView::tt('report_builder_80')
+            );
+        }
+
+        if (count($links)===0) return;
+
         echo '<script type="text/javascript">';
         echo '  /* '.$this->PREFIX.' '.$this->VERSION.': links to logging pages */';
         echo '  $(document).ready(function(){';
         foreach ($links as $link) {
-            echo "    $('#recordActionDropdown').append('$link');";
+            $linkMarkup = str_replace(static::HREFMARK, $link['href'], str_replace(static::LABELMARK, $link['label'], static::LINK_MARKUP));
+            echo "    $('#recordActionDropdown').append('$linkMarkup');";
         }
         echo '  });';
         echo '</script>';
